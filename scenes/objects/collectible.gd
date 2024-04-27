a4c552a6701822d48ea818ac228f037205371a36
Vitordavi-marques <vitordavi.marques@gmail.com>
2024-04-26 23:12:24 -0300

    Some level design work

--- scenes/objects/collectible.gd
+++ scenes/objects/collectible.gd
@@ -1,16 +1,18 @@
 extends Area2D
 class_name Collectible
 
+var collected : bool = false
 
 func _ready() -> void:
 	Signals.level_ready.connect(func(level): level.register_collectible(self))
 	area_entered.connect(on_area_entered)
 
 func on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("player") and visible:
+	if area.is_in_group("player") and not collected:
 		collect()
 
 func collect() -> void:
+	collected = true
 	$AnimationPlayer.play("collect")
 	Signals.item_collected.emit(self)
 	AudioManager.gem_pickup_sfx.play()
